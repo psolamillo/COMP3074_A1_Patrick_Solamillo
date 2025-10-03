8aed6a70c17210908a917cf2847139780d78856f
Patrick Solamillo <patricksolamillo@proton.me>
2025-10-03 13:18:56 -0400

    Add basic pay calculation functionality with test display toast

--- app/src/main/java/com/example/a1_patrick_solamillo/MainActivity.java
+++ app/src/main/java/com/example/a1_patrick_solamillo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a1_patrick_solamillo;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public float hoursWorked;
+    public double hourlyRate;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
 
+
+        });
+        testToast();
+    }
+
+    public double calculatePay(int numHours,double rate)
+    {
+        double regularPay=0;
+        double overtimePay=0;
+        double totalPay;
+        if(numHours <= 40)
+        {
+            regularPay = numHours * rate;
+        }
+        else
+        {
+            overtimePay=(numHours-40)*rate*1.5 + 40*rate;
+        }
+
+        totalPay=regularPay+overtimePay;
+        return totalPay;
+    };
+
+    public void testToast()
+    {
+        String msg = String.valueOf(calculatePay(5,10));
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
