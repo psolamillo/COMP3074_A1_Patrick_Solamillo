a8fbf608183216a79af02ef25eebc66bd7f8a66c
Patrick Solamillo <patricksolamillo@proton.me>
2025-10-04 21:39:33 -0400

    add detailsActivity and details toolbar

--- app/src/main/java/com/example/a1_patrick_solamillo/MainActivity.java
+++ app/src/main/java/com/example/a1_patrick_solamillo/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
